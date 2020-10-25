889f6b775c70ed4a00a16ee008491a7270037143
Toni Vapalo <toni.vapalo@gmail.com>
2020-10-25 18:37:48 +0200

    Pientä lisäystä

--- harjoittelu/src/harjoittelu1/HenkiloMapOhjelma.java
+++ harjoittelu/src/harjoittelu1/HenkiloMapOhjelma.java
@@ -1,9 +1,30 @@
 package harjoittelu1;
 
+import java.util.Scanner;
+
 public class HenkiloMapOhjelma {
 
+	public void valikko() {
+		Scanner input = new Scanner(System.in);
+		int valinta = -1;
+
+		do {
+			System.out.println("\n1. Lisää henkilö ");
+			System.out.println("2. Näytä henkilön tiedot ");
+			System.out.println("3. Muuta henkilön nimi ja osoite ");
+			System.out.println("4. Muuta henkilön koko ");
+			System.out.println("5. Näytä kaikki henkilöt ");
+			System.out.println("0. Lopetus");
+			System.out.print("Anna valintasi (0-5): ");
+			valinta = input.nextInt();
+
+			switch (valinta) {
+
+			}
+		} while (valinta != 0);
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Uusi aloitus");
 	}
 
 }
