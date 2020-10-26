b1a4026c0b48f5a1afc0cba4efcea79fad950460
Toni Vapalo <toni.vapalo@gmail.com>
2020-10-26 10:58:54 +0200

    Puoliväli ensimmäisestä tehtävästä

--- harjoittelu/src/harjoittelu1/HenkiloMapOhjelma.java
+++ harjoittelu/src/harjoittelu1/HenkiloMapOhjelma.java
@@ -1,8 +1,17 @@
 package harjoittelu1;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Scanner;
+import java.util.Set;
 
 public class HenkiloMapOhjelma {
+	private Lukija lukija = new Lukija();
+	private HashMap<String, Henkilo> henkilot = new HashMap<String, Henkilo>();
+
+	public HenkiloMapOhjelma() {
+		valikko();
+	}
 
 	public void valikko() {
 		Scanner input = new Scanner(System.in);
@@ -19,12 +28,40 @@ public class HenkiloMapOhjelma {
 			valinta = input.nextInt();
 
 			switch (valinta) {
-
+			case 1:
+				lisaaHenkilo();
+				break;
+			case 2:
+				naytaHloTiedot();
 			}
 		} while (valinta != 0);
 	}
 
+	public void lisaaHenkilo() {
+		Henkilo hlo = new Henkilo();
+		Koko koko = new Koko();
+		hlo.setNimi(lukija.lueTeksti("Anna nimi: "));
+		if (henkilot.containsKey(hlo.getNimi())) {
+			System.out.println("Olet jo lisännyt henkilön nimellä: " + henkilot.get(hlo.getNimi()));
+			return;
+		}
+		hlo.setOsoite(lukija.lueTeksti("Anna osoite: "));
+		koko.setPituus(lukija.lueDesimaaliluku("Anna pituus: "));
+		koko.setPaino(lukija.lueKokonaisluku("Anna paino: "));
+		hlo.setKoko(koko);
+		henkilot.put(hlo.getNimi(), hlo);
+	}
+
+	public void naytaHloTiedot() {
+		Set<String> nimet = henkilot.keySet();
+		Iterator<String> i = nimet.iterator();
+		while (i.hasNext()) {
+			System.out.println(henkilot.get(i.next()));
+		}
+	}
+
 	public static void main(String[] args) {
+		new HenkiloMapOhjelma();
 	}
 
 }
