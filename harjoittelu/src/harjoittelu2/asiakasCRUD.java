496ab08857b34e6b4063e9c13ce8fbe117190ae8
Toni Vapalo <toni.vapalo@gmail.com>
2020-11-04 15:21:01 +0200

    Muutos valmis

--- harjoittelu/src/harjoittelu2/asiakasCRUD.java
+++ harjoittelu/src/harjoittelu2/asiakasCRUD.java
@@ -82,23 +82,25 @@ public class asiakasCRUD {
 						System.out.print(rs.getString(4) + "\t\t");
 						System.out.println();
 
-					}System.out.println();
-				}con.close();
+					}
+					System.out.println();
+				}
+				con.close();
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void lisaaAsiakas() {
 		String etunimi = lukija.lueTeksti("Anna lisättävän asiakkaan etunimi: ");
 		String sukunimi = lukija.lueTeksti("Anna lisättävän asiakkaan sukunimi: ");
 		String puhelin = lukija.lueTeksti("Anna lisättävän asiakkaan puhelinnumero: ");
 		String sposti = lukija.lueTeksti("Anna lisättävän asiakkaan sähköposti: ");
-		
+
 		if (etunimi.length() > 1 && sukunimi.length() > 1 && puhelin.length() > 1 && sposti.length() > 1) {
 			sql = "INSERT INTO asiakkaat (etunimi, sukunimi, puhelin, sposti) VALUES (?,?,?,?)";
-			
+
 			try {
 				con = yhdista();
 				stmtPrep = con.prepareStatement(sql);
@@ -107,15 +109,71 @@ public class asiakasCRUD {
 				stmtPrep.setString(3, puhelin);
 				stmtPrep.setString(4, sposti);
 				stmtPrep.executeUpdate();
-				
+
 				con.close();
-				
+
 				System.out.println("Asiakkaan lisääminen onnistui");
 				listaaAsiakas();
-			}catch (SQLException e) {
+			} catch (SQLException e) {
 				System.out.println("Asiakkaan lisääminen epäonnistui");
 				e.printStackTrace();
 			}
+		}
+	}
+
+	public void muutaAsiakas() {
+		listaaAsiakas();
+
+		String etunimi = lukija.lueTeksti("Anna muutettavan asiakkaan etunimi: ");
+		String sukunimi = lukija.lueTeksti("Anna muutettavan asiakkaan sukunimi: ");
+
+		sql = "SELECT * FROM asiakkaat WHERE etunimi = ? && sukunimi = ?";
+
+		try {
+			con = yhdista();
+			if (con != null) {
+				stmtPrep = con.prepareStatement(sql);
+				stmtPrep.setString(1, etunimi);
+				stmtPrep.setString(2, sukunimi);
+				rs = stmtPrep.executeQuery();
+
+				if (rs.isBeforeFirst()) { // Jos kyselyllä löytyi tietoa
+					String uusienimi = lukija.lueTeksti("Anna uusi etunimi (enter ohittaa): ");
+					String uusisnimi = lukija.lueTeksti("Anna uusi sukunimi (Enter ohittaa): ");
+					String puhelin = lukija.lueTeksti("Anna uusi puhelinnumero (Enter ohittaa): ");
+					String sposti = lukija.lueTeksti("Anna uusi sähköposti (Enter ohittaa): ");
+
+					if (uusienimi.equals("")) {
+						uusienimi = etunimi;
+					}
+					if (uusisnimi.equals("")) {
+						uusisnimi = sukunimi;
+					}
+					if (puhelin.equals("")) {
+						puhelin = rs.getString("puhelin");
+					}
+					if (sposti.contentEquals("")) {
+						sposti = rs.getString("sposti");
+					}
+
+					sql = "UPDATE asiakkaat SET etunimi = ?, sukunimi = ?, puhelin = ?, sposti = ? WHERE etunimi = ? && sukunimi = ?";
+
+					stmtPrep = con.prepareStatement(sql);
+					stmtPrep.setString(1, uusienimi);
+					stmtPrep.setString(2, uusisnimi);
+					stmtPrep.setString(3, puhelin);
+					stmtPrep.setString(4, sposti);
+					stmtPrep.setString(5, etunimi);
+					stmtPrep.setString(6, sukunimi);
+					stmtPrep.executeUpdate();
+				} else {
+					System.out.println("Antamaasi henkilöä ei ole tietokannassa\n");
+				}
+				con.close();
+				listaaAsiakas();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
