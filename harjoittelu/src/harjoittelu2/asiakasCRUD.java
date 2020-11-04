bb21a34e68dbca47176f0d25cf22c4940109638c
Toni Vapalo <toni.vapalo@gmail.com>
2020-11-04 14:32:19 +0200

    Checkpoint

--- harjoittelu/src/harjoittelu2/asiakasCRUD.java
+++ harjoittelu/src/harjoittelu2/asiakasCRUD.java
@@ -1,6 +1,36 @@
 package harjoittelu2;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import harjoittelu1.Lukija;
+
 public class asiakasCRUD {
+	
+	private Connection con = null;
+	private ResultSet rs = null;
+	private PreparedStatement stmtPrep = null;
+	private String sql;
+	private String db = "Myynti.sqlite";
+	private Lukija lukija = new Lukija();
+	
+	private Connection yhdista() {
+		Connection con = null;
+		String path = System.getProperty("user.dir") + "/";
+		String url = "jdbc:sqlite:" + path + db;
+		
+		try {
+			Class.forName("org.sqlite.JDBC");
+			con = DriverManager.getConnection(url);
+			//System.out.print("Yhteys avattu");
+		} catch (Exception e) {
+			System.out.println("Yhteyden avaus epäonnistui");
+			e.printStackTrace();
+		}
+		return con;
+	}
 
 	public static void main(String[] args) {
 		
