9362fdea6bcd5084b87a1acb791123de660da813
Toni Vapalo <toni.vapalo@gmail.com>
2020-11-19 13:54:31 +0200

    Dao valmis

--- etatehtava4/src/model/dao/Dao.java
+++ etatehtava4/src/model/dao/Dao.java
@@ -1,9 +1,14 @@
 package model.dao;
 
+import java.sql.DriverManager;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
-import com.sun.jdi.connect.spi.Connection;
+import model.Asiakas;
+
+import java.sql.Connection;
 
 public class Dao {
 	private Connection con = null;
@@ -11,4 +16,49 @@ public class Dao {
 	private PreparedStatement stmtPrep = null;
 	private String sql;
 	private String db = "Myynti.sqlite";
+
+	private Connection yhdista() {
+		Connection con = null;
+		String path = System.getProperty("user.dir") + "/";
+		String url = "jdbc:sqlite:" + path + db;
+
+		try {
+			Class.forName("org.sqlite.JDBC");
+			con = DriverManager.getConnection(url);
+		} catch (Exception e) {
+			System.out.println("Yhdistäessä meni jotain pieleen. Yritä uudelleen");
+			e.printStackTrace();
+		}
+
+		return con;
+	}
+
+	public ArrayList<Asiakas> listaaKaikki() {
+		ArrayList<Asiakas> asiakkaat = new ArrayList<Asiakas>();
+		sql = "SELECT * FROM asiakkaat";
+
+		try {
+			con = yhdista();
+			if (con != null) {
+				stmtPrep = con.prepareStatement(sql);
+				rs = stmtPrep.executeQuery();
+				if (rs != null) {
+					while (rs.next()) {
+						Asiakas asiakas = new Asiakas();
+						asiakas.setEtunimi(rs.getString(1));
+						asiakas.setSukunimi(rs.getString(2));
+						asiakas.setPuhelin(rs.getString(3));
+						asiakas.setSposti(rs.getString(4));
+
+						asiakkaat.add(asiakas);
+
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return asiakkaat;
+	}
+
 }
